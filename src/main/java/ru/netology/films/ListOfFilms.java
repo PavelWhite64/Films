39a35d53cbe1c10db5388dda26dd69f24bae9349
Pavel <Test@mail.ru>
2023-04-02 18:27:28 +0400

    initial commit

---
+++ src/main/java/ru/netology/films/ListOfFilms.java
@@ -0,0 +1,43 @@
+package ru.netology.films;
+
+public class ListOfFilms {
+    private String[] films = new String[0];
+    private int limit;
+
+    public ListOfFilms() {
+        this.limit = 5;
+    }
+
+    public ListOfFilms(int limit) {
+        this.limit = limit;
+    }
+
+    public void add(String film) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public String[] findall() {
+        return films;
+    }
+
+    public String[] findLast() {
+        int totalFilms;
+
+        if (films.length < 5) {
+            totalFilms = films.length;
+        } else {
+            totalFilms = limit;
+        }
+
+        String[] tmp = new String[totalFilms];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
+}
